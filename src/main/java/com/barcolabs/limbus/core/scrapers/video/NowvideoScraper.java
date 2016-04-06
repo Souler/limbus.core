7c1a9829f967e907a9274bec29be47cf7d3e5d31
Juan José <soulerhyd@gmail.com>
2016-04-07 01:38:51 +0200

    Updated Nowvideo

--- src/main/java/com/barcolabs/limbus/core/scrapers/video/NowvideoScraper.java
+++ src/main/java/com/barcolabs/limbus/core/scrapers/video/NowvideoScraper.java
@@ -2,6 +2,7 @@ package com.barcolabs.limbus.core.scrapers.video;
 
         import com.barcolabs.limbus.core.exceptions.ScrapingException;
         import com.barcolabs.limbus.core.exceptions.UnexpectedStructureException;
+        import com.barcolabs.limbus.core.scrapers.VideoSiteScraper;
         import com.barcolabs.limbus.core.scrapers.video.base.GetAndPostVideoScraper;
         import com.barcolabs.limbus.core.scrapers.video.js.ScrapingJavaScriptEngine;
 
@@ -10,22 +11,20 @@ package com.barcolabs.limbus.core.scrapers.video;
         import org.jsoup.nodes.Document;
         import org.jsoup.nodes.Element;
         import org.jsoup.select.Elements;
+        import sun.misc.Regexp;
 
         import java.io.IOException;
         import java.net.MalformedURLException;
         import java.net.URL;
+        import java.util.regex.Matcher;
+        import java.util.regex.Pattern;
 
 /**
  * Created by barbosa on 1/09/15.
  */
-public class NowvideoScraper extends GetAndPostVideoScraper {
+public class NowvideoScraper extends VideoSiteScraper {
 
     private final static String API_ENDPOINT = "http://www.nowvideo.sx/api/player.api.php";
-
-    private final static String[] swfObjectMethods ={
-            "createCSS",
-            "embedSWF"
-    };
 
     private final static String[][] API_PARAMS = {
             { "key", "fkzd" },
@@ -38,35 +37,17 @@ public class NowvideoScraper extends GetAndPostVideoScraper {
             { "numOfErrors", "0" }
     };
 
-    private final String[] HOSTNAMES = {
-            "www.nowvideo.sx",
-            "www.nowvideo.ch"
-    };
-
     @Override
     public String getHandledHost() {
-        return HOSTNAMES[0];
+        return "www.nowvideo.sx";
     }
 
-    @Override
-    public boolean canHandle(String uri) {
-        // Override the super implementation for allow to check for two hostnames
-        for (String hostname : HOSTNAMES) {
-            try {
-                URL url = new URL(uri);
-                if (url.getHost().equals(hostname))
-                    return true;
-            } catch (MalformedURLException e) {}
-        }
-        return false;
-
-    }
-
-    private ScrapingJavaScriptEngine prepareEngine(Document doc) throws ScrapingException {
+    private String[][] findApiParams(Document doc) throws ScrapingException {
         Elements scripts = doc.select("script");
         Element targetScript = null;
+
         for (Element script : scripts) {
-            if (script.html().indexOf("flashvars") >= 0) {
+            if (script.html().contains("flashvars")) {
                 targetScript = script;
                 break;
             }
@@ -75,30 +56,34 @@ public class NowvideoScraper extends GetAndPostVideoScraper {
         if (targetScript == null)
             throw new UnexpectedStructureException("Couldn't find eval script tag.");
 
-        ScrapingJavaScriptEngine engine = new ScrapingJavaScriptEngine();
-        String scriptToEval = targetScript.html().replaceAll("<!--.*-->", "");
-        engine.eval(scriptToEval);
+        Pattern keyRegex = Pattern.compile("var fkzd=\"(.*?)\"");
+        Pattern fileRegex = Pattern.compile("flashvars.file=\"(.*?)\";");
 
-        return engine;
+        Matcher keyMatcher = keyRegex.matcher(targetScript.html());
+        Matcher fileMatcher = fileRegex.matcher(targetScript.html());
+
+        if (!keyMatcher.find())
+            throw new UnexpectedStructureException("Couldn't find eval script tag.");
+
+        if (!fileMatcher.find())
+            throw new UnexpectedStructureException("Couldn't find file for nowvideo api.");
+
+        return new String[][] {
+                new String[] { "key", keyMatcher.group(1) },
+                new String[] { "file", fileMatcher.group(1) },
+        };
     }
 
-    private String getVideoUrlFromApi(ScrapingJavaScriptEngine engine) throws IOException, ScrapingException {
+    private String getVideoUrlFromApi(String[][] params) throws IOException, ScrapingException {
         Connection conn = Jsoup.connect(API_ENDPOINT)
                 .ignoreContentType(true)
                 .userAgent(USER_AGENT)
                 .followRedirects(true);
 
-        for (String[] param : API_PARAMS) {
-            String apiParam = param[0];
-            String engineParam = param[1];
-            String value = "undefined";
-            Object valueObj = null;
-            try {
-                valueObj = engine.eval(engineParam);
-            } catch (RuntimeException e) {}
-            if (valueObj != null)
-                value = valueObj.toString();
-            conn.data(apiParam, value);
+        for (String[] param : params) {
+            String key = param[0];
+            String value = param[1];
+            conn.data(key, value);
         }
 
         Connection.Response response = conn.execute();
@@ -114,14 +99,21 @@ public class NowvideoScraper extends GetAndPostVideoScraper {
         throw new ScrapingException("No url param found on API respone");
     }
 
-    @Override
-    protected String parsePostedDocument(Document doc) throws ScrapingException {
-        ScrapingJavaScriptEngine engine = prepareEngine(doc);
+    protected Document getDocument(String location) throws IOException, ScrapingException {
+        Connection.Response response= Jsoup.connect(location)
+                .referrer(location)
+                .ignoreContentType(true)
+                .userAgent(USER_AGENT)
+                .followRedirects(true)
+                .execute();
 
-        try {
-            return getVideoUrlFromApi(engine);
-        } catch (IOException e) {
-            throw new ScrapingException("Nowvideo: Couldn't get video from internal API");
-        }
+        return response.parse();
+    }
+
+    @Override
+    public String get(String uri) throws IOException, ScrapingException {
+        Document doc = getDocument(uri);
+        String[][] params = findApiParams(doc);
+        return getVideoUrlFromApi(params);
     }
 }
